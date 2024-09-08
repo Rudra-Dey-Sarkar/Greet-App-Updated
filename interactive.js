2f5140bba47e87b3d725851da8907e46dbe0f004
Rudra Dey Sarkar <97943164+Rudra-Dey-Sarkar@users.noreply.github.com>
2024-09-08 18:55:13 +0530

    Add files via upload

---
+++ interactive.js
@@ -0,0 +1,18 @@
+function Greet(){
+    const userInput = document.getElementById("user-input");
+    const msg = document.getElementById("msg");
+    const err = document.getElementById("err");
+    msg.innerText=``;
+    msg.style.opacity = 0;
+    err.innerText=``; 
+    err.style.opacity = 0;
+
+    if(userInput.value === ""){
+        err.innerText=`Please Enter Your Name !`;
+        err.style.opacity = 1;
+    }else{
+        msg.innerText=`Welcome ${userInput.value}`;
+        msg.style.opacity = 1;
+    }
+    
+}
